1eb4e12352c19ee15cc6c0a220eb07b3d6f13f56
DCampodonico <d.campodonico@hotmail.com>
2017-03-09 01:49:19 -0300

    Cambiados colores del tema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
-      StatusBar.backgroundColorByHexString('#4A148C');
+      StatusBar.backgroundColorByHexString('#1A237E');
       Splashscreen.hide();
     });
   }
